a3bb65b98a5e9e225908099ec619d0d7d3e4a2bc
GLAVONA <97589269+GLAVONA@users.noreply.github.com>
2023-01-02 16:54:10 +0200

    deleteEduButton

--- src/App.js
+++ src/App.js
@@ -10,30 +10,46 @@ class App extends React.Component {
     super();
     this.state = {
       educationList: [
-        { institution: "Harvard", major: "CS", years: "2012-2016" },
-        { institution: "Harvard1", major: "CS", years: "2012-2016" },
-        { institution: "Harvard2", major: "CS", years: "2012-2016" },
+        { institution: "Harvard", major: "CS", years: "2012-2016", visible:false},
+        { institution: "Harvard1", major: "CS", years: "2012-2016",visible:false},
+        { institution: "Harvard2", major: "CS", years: "2012-2016",visible:false },
       ],
       workExperienceList: [],
     };
     this.check = this.check.bind(this);
     this.updateEduValue = this.updateEduValue.bind(this);
+    this.deleteEdu = this.deleteEdu.bind(this);
   }
 
   check() {
     console.log(this.state.educationList);
   }
 
-  updateEduValue(e){
+  updateEduValue(e) {
     const newArr = this.state.educationList;
-    const index = Array.from(e.target.parentElement.parentElement.children).indexOf(e.target.parentElement)
+    const index = Array.from(
+      e.target.parentElement.parentElement.parentElement.children
+    ).indexOf(e.target.parentElement.parentElement);
     const tarName = e.target.name;
     newArr[index][tarName] = e.target.value;
     this.setState({
       ...this.state,
       educationList: newArr,
-    })
+    });
   }
+
+  deleteEdu(e) {
+    const newArr = this.state.educationList;
+    const index = Array.from(
+      e.target.parentElement.parentElement.children
+    ).indexOf(e.target.parentElement);
+    newArr.splice(index, 1);
+    this.setState({
+      ...this.state,
+      educationList: newArr,
+    });
+  }
+  
 
   render() {
     return (
@@ -44,15 +60,26 @@ class App extends React.Component {
           <GeneralInfo />
         </div>
         <div className="main header">
-        <div className="education">
-          {this.state.educationList.map((educationObject, index) => (
-            <Education
-              institution={educationObject.institution}
-              major={educationObject.major}
-              years={educationObject.years}
-              change={this.updateEduValue}
-            />
-          ))}
+          <div className="education">
+            Education
+            {this.state.educationList.map((educationObject) => (
+              <div className="educationContainer" >
+                <Education
+                  institution={educationObject.institution}
+                  major={educationObject.major}
+                  years={educationObject.years}
+                  change={this.updateEduValue}
+                />
+                {
+                  <button
+                    onClick={(e) => this.deleteEdu(e)}
+                    className="deleteButton"
+                  >
+                    X
+                  </button>
+                }
+              </div>
+            ))}
           </div>
         </div>
       </div>
